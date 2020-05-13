157bc5809a703d6a1b3014efc0d098712aa3ea66
Taylor <tdy721@gmail.com>
2020-05-13 13:47:43 -0700

    good

--- index.js
+++ index.js
@@ -78,6 +78,12 @@ server.route({
 server.route({
     method: "GET",
     path: "/deck",
+    config: {
+        cors: {
+            origin: ["*"],
+            additionalHeaders: ["cache-control", "x-requested-with"],
+        },
+    },
     handler: async (request, h) => {
         const deckID = uuidv4();
         try {
@@ -88,6 +94,182 @@ server.route({
         // console.log("redirect to deck uuid");
         return h.redirect(`/deck/${deckID}`);
         // return '<pre>' + JSON.stringify(getNewCards(), null, 2) +'</pre>' ;
+    },
+});
+
+server.route({
+    method: "GET",
+    path: "/cli",
+    config: {
+        cors: {
+            origin: ["*"],
+            additionalHeaders: ["cache-control", "x-requested-with"],
+        },
+    },
+    handler: async (request, h) => {
+        const deckID = uuidv4();
+        try {
+            await tedis.set(`deck-${deckID}`, JSON.stringify(getNewCards()));
+        } catch (err) {
+            console.error(err);
+        }
+        // console.log("redirect to deck uuid");
+        return h.redirect(`/cli/deck/${deckID}/cards/5`);
+        // return '<pre>' + JSON.stringify(getNewCards(), null, 2) +'</pre>' ;
+    },
+});
+
+server.route({
+    method: "GET",
+    path: "/cli/deck/{deckID}/cards/{number}",
+    config: {
+        cors: {
+            origin: ["*"],
+            additionalHeaders: ["cache-control", "x-requested-with"],
+        },
+    },
+    handler: async function (request, h) {
+        console.log("add uuid, get some cards from deck");
+        console.log("assign uuid to hand");
+        const { deckID } = request.params;
+        let validDeckId;
+        try {
+            validDeckId = await tedis.exists(`deck-${deckID}`);
+        } catch (err) {
+            console.log(error);
+        }
+        if (validDeckId) {
+            const number = parseInt(request.params.number, 10);
+            let deck;
+            try {
+                deck = JSON.parse(await tedis.get(`deck-${deckID}`));
+            } catch (err) {
+                console.error(err);
+            }
+            let deckArray = Object.values(deck);
+            const hand = new Array(number)
+                .join()
+                .split(",")
+                .map((i) => {
+                    return deckArray.shift();
+                });
+            // console.log( hand, deckArray );
+            try {
+                await tedis.set(`deck-${deckID}`, JSON.stringify(deckArray));
+                await tedis.expire(`deck-${deckID}`, EXPIRE_BUMP);
+            } catch (err) {
+                console.error(err);
+            }
+            // console.log(deckArray.length, typeof deckArray );
+            // console.log(number, deckID, hand);
+            //         return (
+            //             "<pre>" +
+            //             JSON.stringify(hand, null, 2) +
+            //             `cards left: ${deckArray.length}` +
+            //             "</pre>"
+            //         );
+            return (
+                `#!/bin/bash
+echo ${hand.join(" ")}
+hand=${hand.join("")}
+remainingHand=''
+host=localhost:3000
+drawURL=$host/cli/deck/${deckID}/
+
+read -p "1-5 to hold: " holdcards
+# echo Hold These Cards? $holdcards
+echo Holding These Cards: $holdcards
+numberHeld=` +
+                "${#holdcards}" +
+                `
+for (( i=0; i<numberHeld; i++ )); do
+  echo ` +
+                '"${hand:$i*2:2}"' +
+                `
+  echo `+'"${holdcards:$i:1}"'+`
+  remainingHand=$remainingHand` +
+                '"${hand:$i*2:2}"' +
+                `
+done
+#echo $remainingHand
+numberToDraw=$(expr 5 - $numberHeld)
+drawURL=$drawURL$remainingHand/draw/$numberToDraw
+echo $drawURL
+drawCards=$(curl -s $drawURL)
+finalHand=$remainingHand$drawCards
+echo $finalHand
+echo @TODO: Score Me!
+echo @TODO: Play Again?
+`
+            );
+        } else {
+            return "expired?";
+        }
+    },
+});
+
+server.route({
+    method: "GET",
+    path: "/cli/deck/{deckID}/draw/{draw_number}/{hand}",
+    config: {
+        cors: {
+            origin: ["*"],
+            additionalHeaders: ["cache-control", "x-requested-with"],
+        },
+    },
+    handler: async function (request, h) {
+        console.log("cli draw - get some cards from deck");
+        const { deckID, hand, draw_number } = request.params;
+        // Todo, sanitize hand input, with deck.js utility function
+        let validDeckId;
+        try {
+            validDeckId = await tedis.exists(`deck-${deckID}`);
+        } catch (err) {
+            console.log(error);
+        }
+        if (validDeckId) {
+            const draw_number_clean = parseInt(draw_number, 10);
+            let deck;
+            try {
+                deck = JSON.parse(await tedis.get(`deck-${deckID}`));
+            } catch (err) {
+                console.error(err);
+            }
+            let deckArray = Object.values(deck);
+            const hand = new Array(draw_number_clean)
+                .join()
+                .split(",")
+                .map((i) => {
+                    return deckArray.shift();
+                });
+            // console.log( hand, deckArray );
+            try {
+                await tedis.set(`deck-${deckID}`, JSON.stringify(deckArray));
+                await tedis.expire(`deck-${deckID}`, EXPIRE_BUMP);
+            } catch (err) {
+                console.error(err);
+            }
+            // console.log(deckArray.length, typeof deckArray );
+            // console.log(number, deckID, hand);
+            //         return (
+            //             "<pre>" +
+            //             JSON.stringify(hand, null, 2) +
+            //             `cards left: ${deckArray.length}` +
+            //             "</pre>"
+            //         );
+            return hand.join("");
+            // `#!/bin/bash
+            // echo ${hand.join(' ')}
+            // read -p "1-5 to hold: " holdcards
+            // # echo Hold These Cards? $holdcards
+            // echo Holding These Cards: $holdcards
+            // ` + "for (( i=0; i<${#holdcards}; i = i+1 )); do" + `
+            //   echo ` + '"${holdcards:$i:1}"' + `
+            // done
+            // `;
+        } else {
+            return "expired?";
+        }
     },
 });
 
@@ -121,6 +303,12 @@ server.route({
 server.route({
     method: "GET",
     path: "/deck/{deckID}",
+    config: {
+        cors: {
+            origin: ["*"],
+            additionalHeaders: ["cache-control", "x-requested-with"],
+        },
+    },
     handler: async (request, reply) => {
         // !! VALIDATE THE UUIDv4
         const { deckID } = request.params;
@@ -155,6 +343,12 @@ server.route({
 server.route({
     method: "GET",
     path: "/deck/{deckID}/cards/{number}",
+    config: {
+        cors: {
+            origin: ["*"],
+            additionalHeaders: ["cache-control", "x-requested-with"],
+        },
+    },
     handler: async function (request, h) {
         console.log("add uuid, get some cards from deck");
         console.log("assign uuid to hand");
